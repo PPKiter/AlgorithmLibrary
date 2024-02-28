d731e5675d79aba4674d458fae4dda419b9ed3dd
PPKiter <1026762773@qq.com>
2024-02-28 22:47:26 +0800

    回溯法

---
+++ MyAlgorithm/04_Backtrack/BacktrackAlgo.cs
@@ -0,0 +1,142 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Backtrack
+{
+    internal class BacktrackAlgo<T> where T : class
+    {
+        /// <summary>
+        /// 冲突检查：用于剪枝函数中的剪枝操作
+        /// </summary>
+        /// <typeparam name="S"></typeparam>
+        /// <param name="existing">个体1，前序</param>
+        /// <param name="candidate">个体2，当前</param>
+        /// <returns></returns>
+        public delegate bool ConflictCheckerDelegate<S>(S existing, S candidate);
+
+        private int _max;
+        //分组列表
+        private List<List<T>> _groups;
+
+        /// <summary>
+        /// 当前状态下的一条路径结果
+        /// </summary>
+        private Stack<T> Result { get; set; }
+
+        public List<List<T>> Results { get; set; } = new List<List<T>>();
+
+        /// <summary>
+        /// 冲突函数
+        /// </summary>
+        private ConflictCheckerDelegate<T> ConflictChecker { get; set; }
+
+        Func<T, T, bool> Conflict2 { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="groups">待回溯列表，第一层list是所有分组，第二层list是每个分组有多少个体</param>
+        /// <param name="conflictChecker"></param>
+        //public BacktrackAlgo(List<List<T>> groups, ConflictCheckerDelegate<T> conflictChecker)
+        //{
+        //    _max = groups.Count - 1;
+        //    _groups = groups;
+        //    ConflictChecker = conflictChecker;
+        //}
+
+        public BacktrackAlgo(List<List<T>> groups, Func<T, T, bool> conflict2)
+        {
+            _max = groups.Count - 1;
+            _groups = groups;
+            //ConflictChecker = conflictChecker;
+            Conflict2 = conflict2;
+        }
+
+        /// <summary>
+        /// 算法入口
+        /// </summary>
+        /// <param name="col"></param>
+        public void Backtrack(int col)
+        {
+            //达到最后一列分组，返回
+            if (col == _max)
+            {
+                return;
+            }
+            foreach (var group in _groups[col])
+            {
+                Result = new Stack<T>();
+                col = 0;
+                Result.Push(group);
+                Check(ref col);
+            }
+        }
+
+        /// <summary>
+        /// 检查每一项
+        /// </summary>
+        /// <param name="start"></param>
+        private void Check(ref int start)
+        {
+            if (start == _max)
+            {
+                //按顺序输出
+                var result = Result.Reverse().Cast<T>().ToList();
+                Results.Add(result);
+                Result.Pop();
+                start--;
+                return;
+            }
+            var nextGroup = _groups[start + 1];
+            for (int i = 0; i < nextGroup.Count; i++)
+            {
+                if (Judge(nextGroup[i]))
+                {
+                    Result.Push(nextGroup[i]);
+                    start++;
+                    Check(ref start);
+                }
+                //一层遍历完毕后，弹出栈顶元素
+                Result.Pop();
+                start--;
+            }
+        }
+
+        /// <summary>
+        /// 剪枝函数：判断当前元素是否与路径前序结果冲突
+        /// true:检查通过；false：不通过
+        /// </summary>
+        /// <param name="cur"></param>
+        /// <returns></returns>
+        private bool Judge(T cur)
+        {
+            var exists = Result.ToList();
+            foreach (var exist in exists)
+            {
+                //冲突检查
+                //if (ConflictChecker(exist, cur))
+                //{
+                //    return false;
+                //}
+
+                try
+                {
+                    if (Conflict2(exist, cur))
+                    {
+                        return false;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MyAlgorithm/04_Backtrack/BacktrackCmd.cs
@@ -0,0 +1,74 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometry;
+
+namespace _04_Backtrack
+{
+    [Transaction(TransactionMode.Manual)]
+    public class BacktrackCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var regions = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DetailComponents).OfClass(typeof(FilledRegion)).Cast<FilledRegion>().ToList();
+
+            var g1 = regions.Where(p => p.get_Parameter(BuiltInParameter.DOOR_NUMBER).AsString().Contains("1")).ToList();
+            var g2 = regions.Where(p => p.get_Parameter(BuiltInParameter.DOOR_NUMBER).AsString().Contains("2")).ToList();
+            var g3 = regions.Where(p => p.get_Parameter(BuiltInParameter.DOOR_NUMBER).AsString().Contains("3")).ToList();
+
+            var pts1 = g1.Select(p => (p.GetBoundaries()[0].ToList()[0] as Arc).Center).ToList();
+            var pts2 = g2.Select(p => (p.GetBoundaries()[0].ToList()[0] as Arc).Center).ToList();
+            var pts3 = g3.Select(p => (p.GetBoundaries()[0].ToList()[0] as Arc).Center).ToList();
+
+
+            List<List<Line>> groups = new List<List<Line>>();
+            foreach (var p1 in pts1)
+            {
+                List<Line> group1 = new List<Line>();
+                foreach (var p2 in pts2)
+                {
+                    Line ll = Line.CreateBound(p1, p2);
+                    group1.Add(ll);
+                }
+                groups.Add(group1);
+            }
+            ////BacktrackAlgo<Line> algo = new BacktrackAlgo<Line>(groups, IsConflict);
+            BacktrackAlgo<Line> algo = new BacktrackAlgo<Line>(groups, IsConflict);
+            algo.Backtrack(0);
+            doc.DrawDebugCurves(algo.Results[0]);
+
+
+            return Result.Succeeded;
+        }
+
+        public bool IsConflict(Line l1, Line l2)
+        {
+            l1.Intersect(l2, out IntersectionResultArray results);
+            //没有交集，不冲突
+            if (results == null || results.IsEmpty)
+            {
+                return false;
+            }
+            ////有交集，但是交点为自身端点，不冲突
+            //if (results.Size == 1)
+            //{
+            //    var insect = results.get_Item(0).XYZPoint;
+            //    var isEnd = insect.IsAlmostEqualTo(l1.GetEndPoint(0), 1e-3) || insect.IsAlmostEqualTo(l1.GetEndPoint(1), 1e-3)
+            //        || insect.IsAlmostEqualTo(l2.GetEndPoint(0), 1e-3) || insect.IsAlmostEqualTo(l2.GetEndPoint(1), 1e-3);
+            //    if (isEnd)
+            //    {
+            //        return false;
+            //    }
+            //}
+            return true;
+        }
+    }
+}
